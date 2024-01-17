d4881746294685e433fb6fbbd5d636e67f21fa40
leezhenxiong <849605526@qq.com>
2024-01-17 22:18:09 +0800

    1

--- yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
+++ yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
@@ -1,6 +1,5 @@
 package cn.iocoder.yudao.server;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -21,7 +20,7 @@ public class YudaoServerApplication {
         // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
         // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
 
-        SpringApplication.run(YudaoServerApplication.class, args);
+        // SpringApplication.run(YudaoServerApplication.class, args);
 //        new SpringApplicationBuilder(YudaoServerApplication.class)
 //                .applicationStartup(new BufferingApplicationStartup(20480))
 //                .run(args);
@@ -29,6 +28,13 @@ public class YudaoServerApplication {
         // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
         // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
         // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
+        System.out.println(hash("dick"));
+        System.out.println(hash(new Object()));
+    }
+
+    static int hash(Object o) {
+        int h;
+        return (o == null) ? 0 : (h = o.hashCode()) ^ (h >>> 16);
     }
 
 }
